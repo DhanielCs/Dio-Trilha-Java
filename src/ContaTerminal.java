efe097b879087c5b29c80dd417427ae1ef9bced8
Dhaniel Sousa <dhoficial2@gmail.com>
2024-08-23 12:12:50 -0300

    Atualização criação da ContaTerminal.java

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner ler = new Scanner(System.in);
+        System.out.println(" Olá Seja Bem-Vindo(a) ao Seu Banco Digital.");
+        System.out.println(" Para Ter Acesso a sua Conta Bancária Preciso de Alguns Dados.");
+
+        String Agencia;
+        int NumeroDaConta = 0;
+        String NomeCliente;
+        double SaldoConta = 0.0;
+
+        System.out.println(" Por Favor Insira sua Agência : ");
+        Agencia = ler.next();
+
+
+
     }
 }
