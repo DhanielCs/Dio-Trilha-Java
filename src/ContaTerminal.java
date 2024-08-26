731bc5d0f6d3012939177ae021d5b5fa2ea0442b
Dhaniel Sousa <dhoficial2@gmail.com>
2024-08-26 09:03:28 -0300

    DIO TRILHA JAVA criação da ContaTerminal.java

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -5,15 +5,25 @@ public class ContaTerminal {
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
         System.out.println(" Olá Seja Bem-Vindo(a) ao Seu Banco Digital.");
-        System.out.println(" Para Ter Acesso a sua Conta Bancária Preciso de Alguns Dados.");
+        System.out.println(" Vamos Iniciar o Cadastro da sua Conta Bancária, Preciso de Alguns Dados para Iniciar.");
 
         String Agencia;
         int NumeroDaConta = 0;
         String NomeCliente;
-        double SaldoConta = 0.0;
+        float SaldoConta = 0;;
 
-        System.out.println(" Por Favor Insira sua Agência : ");
+        System.out.println(" Por Favor ,Insira seu Nome Completo :");
+        NomeCliente = ler.nextLine();
+        System.out.println(" Agora Insira sua Agência : ");
         Agencia = ler.next();
+        System.out.println(" Otimo, por favor Insira sua Conta : ");
+        NumeroDaConta = ler.nextInt();
+        System.out.println(" Para Finalizar , Insira seu  Saldo para adicionar a sua Conta : ");
+        SaldoConta = ler.nextFloat();
+
+        System.out.println(" Olá "+(NomeCliente)+", obrigado por criar uma conta em nosso banco, \n" +
+                            "sua agência é "+(Agencia)+", conta "+(NumeroDaConta)+" e seu saldo R$ "+(SaldoConta)+
+                             " já está disponível para saque.");
 
 
 
